bf925f94bdd89b959ab77026ae12fa8780d69a9d
Gennadiy Sabinin <gsabinin@icloud.com>
2020-12-30 17:42:21 +0300

    add Admin

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admin', function () {
+    return view('admin.index');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
