24de339155292ac5e7e70a3bc97a53dd4c5f8671
Gennadiy Sabinin <gsabinin@icloud.com>
2021-01-13 14:00:40 +0300

    add service and rent block

---
+++ resources/views/blocks/serviceReview.blade.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <div class="service">
+        <div class="d-flex">
+            <div class="col-md-6 service__img"></div>
+            <div class="col-md-6 p-2 d-flex">
+                <div class="service__rightBlock">
+                    <div class="service__decor"></div>
+                    <div class="service__textBlock">
+                        <p class="service__heading">Мы предоставляем различные услуги</p>
+                        <p class="service__text">Многие наши сотрудники трудятся для Вашего блага! Хотите ознакомится со всеми услугами, которые мы предоствляем?</p>
+                        <div class="service__linkBlock">
+                            <a href="#" class="service__link">Наши услуги
+                                <div class="line"></div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,6 @@
 
 @section('content')
     <carousel></carousel>
-    <div class="container">
-        <div class="statistic">
-            <div class="row">
-                <div class="statistic__number p-2 offset-2">15</div>
-                <div class="statistic__text p-2 offset-2">текст</div>
-            </div>
-
-{{--            <div class="statistic__order p-2">заказать</div>--}}
-        </div>
-    </div>
+    @include('blocks.rentPlace')
+    @include('blocks.serviceReview')
 @endsection
