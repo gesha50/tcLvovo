132ab289099a8dd8e456d8342418fd41d7772352
Gennadiy Sabinin <gsabinin@icloud.com>
2021-01-15 16:02:17 +0300

    add footer

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -50,8 +50,69 @@
                 </div>
             </div>
         </div>
-        <div class="footerMiddle">
-            
+        <div class="footerMiddle d-flex justify-content-around">
+            <div class="footerMiddle__block">
+                <div class="footerLinks">
+                    <div class="footerLinks__header">
+                        Быстрые ссылки
+                    </div>
+                    <hr>
+                    <ul class="footerLinks__body">
+                        <li class="footerLinks__li">
+                            <a href="/" class="footerLinks__link">Главная</a>
+                        </li>
+                        <li class="footerLinks__li">
+                            <a href="/" class="footerLinks__link">Home</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footerMiddle__block">
+                <div class="footerHours">
+                    <div class="footerHours__header">
+                        Часы работы
+                    </div>
+                    <hr>
+                    <div class="footerHours__body">
+                        <div class="footerHours__block d-flex justify-content-between">
+                            <div class="footerHours__day flex-shrink-0">Пн - Пт:</div>
+                            <div class="footerHours__line w-100"></div>
+                            <div class="footerHours__time flex-shrink-0">9:00 - 21:00</div>
+                        </div>
+                        <div class="footerHours__block d-flex justify-content-between">
+                            <div class="footerHours__day flex-shrink-0">Суббота:</div>
+                            <div class="footerHours__line w-100"></div>
+                            <div class="footerHours__time flex-shrink-0">9:00 - 20:00</div>
+                        </div>
+                        <div class="footerHours__block d-flex justify-content-between">
+                            <div class="footerHours__day flex-shrink-0">Воскресенье:</div>
+                            <div class="footerHours__line w-100"></div>
+                            <div class="footerHours__time flex-shrink-0">9:00 - 20:00</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footerMiddle__block">
+                <div class="footerEmail">
+                    <div class="footerEmail__header">
+                        Обратная связь
+                    </div>
+                    <hr>
+                    <div class="footerEmail__send">
+                        <form class="d-flex" action="#" method="POST">
+                            <input type="text" class="footerEmail__input"
+                            placeholder="Введите ваш email">
+                            <button class="footerEmail__btn">Отправить</button>
+                        </form>
+                    </div>
+                    <div class="footerEmail__body">
+                        <p>Мы с вами свяжемся в ближайшее время! Спасибо!</p>
+                    </div>
+                    <div class="footerEmail__footer">
+
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="footerDown">
         </div>
